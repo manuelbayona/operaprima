5f43d4107c8fa710c772192d051add3a3c9df294
MBayona <manuel.bayona.adesis@gmail.com>
2015-03-26 13:03:45 +0100

    [ADD] first service

--- src/main/java/com/operaprima/services/dtos/UsersDto.java
+++ src/main/java/com/operaprima/services/dtos/UsersDto.java
@@ -3,95 +3,29 @@ package com.operaprima.services.dtos;
 import java.io.Serializable;
 import java.util.List;
 
-import com.operaprima.commons.dtos.EmailDto;
-
 /**
  * @author Adesis
  *
  */
 public class UsersDto implements Serializable {
 
-	private static final long serialVersionUID = 1122507404235396620L;
+	private static final long serialVersionUID = -4129074881856964270L;
 
-	private String id;
-	private String userName;
-	private String publicPassword;
-	private List<PersonDto> profiles;
-	private EmailDto email;
+	private List<UserDto> users;
 
 	/**
-	 * @return the id
+	 * @return the users
 	 */
-	public String getId() {
-		return id;
+	public List<UserDto> getUsers() {
+		return users;
 	}
 
 	/**
-	 * @param id
-	 *            the id to set
+	 * @param users
+	 *            the users to set
 	 */
-	public void setId(final String id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the userName
-	 */
-	public String getUserName() {
-		return userName;
-	}
-
-	/**
-	 * @param userName
-	 *            the userName to set
-	 */
-	public void setUserName(final String userName) {
-		this.userName = userName;
-	}
-
-	/**
-	 * @return the publicPassword
-	 */
-	public String getPublicPassword() {
-		return publicPassword;
-	}
-
-	/**
-	 * @param publicPassword
-	 *            the publicPassword to set
-	 */
-	public void setPublicPassword(final String publicPassword) {
-		this.publicPassword = publicPassword;
-	}
-
-	/**
-	 * @return the profiles
-	 */
-	public List<PersonDto> getProfiles() {
-		return profiles;
-	}
-
-	/**
-	 * @param profiles
-	 *            the profiles to set
-	 */
-	public void setProfiles(final List<PersonDto> profiles) {
-		this.profiles = profiles;
-	}
-
-	/**
-	 * @return the email
-	 */
-	public EmailDto getEmail() {
-		return email;
-	}
-
-	/**
-	 * @param email
-	 *            the email to set
-	 */
-	public void setEmail(final EmailDto email) {
-		this.email = email;
+	public void setUsers(final List<UserDto> users) {
+		this.users = users;
 	}
 
 }
