c0ffdfb5b49f25e3e2a0e0f9b1bec4ca70519ddc
MBayona <manuel.bayona.adesis@gmail.com>
2015-03-26 16:30:44 +0100

    [ADD] refactor

--- src/main/java/com/operaprima/services/business/dtos/enums/UserStateEnum.java
+++ src/main/java/com/operaprima/services/business/dtos/enums/UserStateEnum.java
@@ -6,6 +6,43 @@ package com.operaprima.services.business.dtos.enums;
  */
 public enum UserStateEnum {
 
-	ACTIVE, DISMISSED;
+	ACTIVE("A"), DISMISSED("D");
+
+	private String code;
+
+	/**
+	 * @param String
+	 */
+	UserStateEnum(final String code) {
+		this.code = code;
+	}
+
+	/**
+	 * @param String
+	 * @return UserStateEnum
+	 */
+	UserStateEnum getByCode(final String code) {
+		for (final UserStateEnum value : values()) {
+			if (code.equals(value.getCode())) {
+				return value;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code
+	 *            the code to set
+	 */
+	public void setCode(final String code) {
+		this.code = code;
+	}
 
 }
